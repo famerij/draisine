670d96b79ee3d4f6ee9bbad5b2506dff52b657f6
Henri <henri.viitanen@gmail.com>
2018-02-10 01:56:07 +0200

    Added parallax

---
+++ Assets/Scripts/GraphicsRepeater.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GraphicsRepeater : MonoBehaviour
+{
+	public SpriteRenderer Graphics;
+
+	public float YMin;
+	public float YMax;
+	public float SpacingMin;
+	public float SpacingMax;
+
+	private readonly List<SpriteRenderer> _graphics = new List<SpriteRenderer>();
+	private Vector3 _originalPosition;
+
+	void Start()
+	{
+		_originalPosition = Graphics.transform.position;
+		var newGraphics = Instantiate(Graphics.gameObject);
+		newGraphics.transform.SetParent(transform, false);
+		newGraphics.transform.position = Graphics.transform.position;
+		_graphics.Add(newGraphics.GetComponent<SpriteRenderer>());
+		Graphics.gameObject.SetActive(false);
+
+		for (int i = 0; i < 50; i++)
+		{
+			AddGraphic();
+		}
+	}
+	
+	void Update()
+	{
+		if (_graphics.Last() != null && _graphics.Last().transform.position.x < 100f)
+		{
+			AddGraphic();
+		}
+		
+		if (_graphics[0].transform.position.x < -100f)
+		{
+			Destroy(_graphics[0].gameObject);
+			_graphics.RemoveAt(0);
+		}	
+	}
+
+	void AddGraphic()
+	{
+		var newGraphics = Instantiate(Graphics.gameObject);
+		newGraphics.SetActive(true);
+		newGraphics.transform.SetParent(transform, false);
+		float x = _graphics.Last().transform.position.x + (_graphics.Last().bounds.size.x - .1f) + 
+		          (Mathf.Approximately(SpacingMax, 0f) ? 0f : Random.Range(SpacingMin, SpacingMax));
+		float y = _originalPosition.y + (Mathf.Approximately(YMax, 0f) ? 0f : Random.Range(YMin, YMax));
+		newGraphics.transform.position = new Vector3(x, y);
+		_graphics.Add(newGraphics.GetComponent<SpriteRenderer>());
+	}
+}
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+	public float Speed;
+	
+	public Transform Far;
+	public Transform Near;
+	public Transform Nearest;
+	public Transform Front;
+	
+	public float FarSpeedMultiplier;
+	public float NearSpeedMultiplier;
+	public float NearestSpeedMultiplier;
+	public float FrontSpeedMultiplier; 
+	
+	void Update()
+	{
+		if (Far != null)
+			Far.transform.position += Vector3.left * Time.deltaTime * Speed * FarSpeedMultiplier;
+		
+		if (Near != null)
+			Near.transform.position += Vector3.left * Time.deltaTime * Speed * NearSpeedMultiplier;
+		
+		if (Nearest != null)
+			Nearest.transform.position += Vector3.left * Time.deltaTime * Speed * NearestSpeedMultiplier;
+		
+		if (Front != null)
+			Front.transform.position += Vector3.left * Time.deltaTime * Speed * FrontSpeedMultiplier;
+	}
+}
