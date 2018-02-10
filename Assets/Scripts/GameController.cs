4a1333a36ed23c5df197a6ef162574d46cf4fb25
Henri <henri.viitanen@gmail.com>
2018-02-10 15:40:52 +0200

    Added GameController for Game scene, Added wheel rotation based on speed

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public float MovementSpeed = 1f;
+	public float WheelSpeedMultiplier = 5f; 
+	
+	[Header("References")]
+	public PuzzleController PuzzleController;
+	public Parallax ParallaxBackground;
+	public List<Transform> Wheels;
+
+	private void Update()
+	{
+		ParallaxBackground.Speed = MovementSpeed;
+		
+		Wheels.ForEach(t => t.Rotate(-Vector3.forward, 2 * Mathf.PI * WheelSpeedMultiplier * MovementSpeed * Time.deltaTime));
+	}
+}
