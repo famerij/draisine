07d999870f3cb0f267ef7a0b034cf5886c2ac70d
Henri <henri.viitanen@gmail.com>
2018-02-11 01:17:49 +0200

    Intro to build

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+	public FadeFromToBlack FadeIn;
+	
+	private IEnumerator Start()
+	{
+		yield return new WaitForSeconds(6f);
+		
+		FadeIn.FadeIn(() =>
+		{
+			SceneManager.LoadScene("Tutorial");
+		});
+	}
+}
---
+++ Assets/Scripts/Screenshot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			ScreenCapture.CaptureScreenshot(string.Format("screen_{0}.png", 
+				DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")));
+		}
+	}
+}
